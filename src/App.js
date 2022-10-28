fdf750f3026e4273895e64339881d518b0263692
Cédric Couton <cedric.couton@doctolib.com>
2022-10-28 15:49:43 +0200

    issue with placeholderdata and enable false

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useQuery, QueryClientProvider, QueryClient} from '@tanstack/react-query'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fetchUsers = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  return res.json();
+};
+
+export default function App(props) {
+  const queryClient = new QueryClient()
+
+  return <QueryClientProvider client={queryClient}><Parent/></QueryClientProvider>;
 }
 
-export default App;
+const Parent = () => {
+
+  const response = useQuery(["users"], fetchUsers, { placeholderData: {}, enabled: false});
+
+  const [state1, setState1] = React.useState(1)
+  const [state2, setState2] = React.useState(10)
+
+  return <div>
+    <Child1 value1={state1} callBackToUpdateParentState={(value) => setState2(value)}/>
+    <Child2 value2={state2}/>
+  </div>
+
+}
+const Child1 = ({value1, callBackToUpdateParentState}) => {
+  const [childValue, setChildValue] = React.useState(1)
+
+  console.log('render Child1')
+
+  React.useEffect(() => {
+    console.log('Child1 component did update')
+    callBackToUpdateParentState(childValue)
+    console.log('callBackToUpdateParentState just called from Child1')
+  })
+
+  return <div>
+    Child1 : {value1}<br/>
+    ChildValue1 : {childValue}<br/>
+    <button onClick={() => {
+      console.log("Click")
+      setChildValue(99)
+    }}>Click</button>
+  </div>
+}
+
+const Child2 = ({value2}) => {
+  console.log('render Child2')
+
+  return <div style={{color: "white"}}>Child2 : {value2}</div>
+}
+
